0c3deb1c9997885efa1aac7b99510fe29ef4ffc7
Jessica <jessica.ayres1107@gmail.com>
2022-02-04 13:23:21 -0500

    review class

--- book_app/src/main/java/Review.java
+++ book_app/src/main/java/Review.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,27 @@
  *
  * @author Jessica
  */
-class Review {
+public class Review {
+    Person person;
+    Book book;
+    ArrayList<Review> review;
+    ArrayList<Club> club;
     
+    //constructor
+    Review (Person person, Book book){
+        this.person = person;
+        this.book = book;
+        review = new ArrayList<>();
+        club = new ArrayList<>();
+    }
+    //methods
+    public void editReview(){
+        
+    }
+    public void addClub(Review club){
+        club.addClub(club);
+    }
+    public void removeClub(Review club){
+        club.removeClub(club);
+    }
 }
