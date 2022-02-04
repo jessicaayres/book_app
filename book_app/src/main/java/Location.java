73fdd7d4b3dadf26722bf0d520db4ab679453c8a
Jessica <jessica.ayres1107@gmail.com>
2022-02-04 14:06:46 -0500

    location class

--- book_app/src/main/java/Location.java
+++ book_app/src/main/java/Location.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,28 @@
  *
  * @author Jessica
  */
-class Location {
+abstract class Location {
+    String name;
+    ArrayList<Book> book;
+    
+    //constructor
+    Location (String name){
+        this.name = name;
+        book = new ArrayList<>();
+    }
+    //methods
+    //need help w these
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void addBook (Location book){
+        book.addBook(book);
+    }
+    public void removeBook (Location book){
+        book.removeBook(book);
+    }
     
 }
