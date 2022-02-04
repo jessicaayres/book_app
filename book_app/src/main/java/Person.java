46e7370654c7b6f6c0c232fd0cc64de979523e64
Jessica <jessica.ayres1107@gmail.com>
2022-02-04 13:01:49 -0500

    this is person class (idk what to do with changeName method)

--- book_app/src/main/java/Person.java
+++ book_app/src/main/java/Person.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,39 @@
  *
  * @author Jessica
  */
-class Person {
+public class Person {
+    String name;
+    int id;
+    ArrayList<Review> review;
+    
+    //constructors
+    Person (String name, int id){
+        this.name = name;
+        this.id = id;
+        review = new ArrayList<>();
+    }
+    //methods
+    public void addReview(Person review){
+        review.addReview(review); 
+    }
+    public void deleteReview(Person review){
+        review.deleteReview(review);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getID (){
+        return this.id;
+    }
+    public void setID(int id){
+        this.id = id;
+    }
+    public void changeName(){
+       //dont know what to put here
+       System.out.println(name);
+    }
     
 }
