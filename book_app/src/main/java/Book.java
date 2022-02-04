a175303ee8b0174665ffe351bf67af1319b243a7
Jessica <jessica.ayres1107@gmail.com>
2022-02-04 13:00:34 -0500

    sorry this is the book class (disregard prior)

--- book_app/src/main/java/Book.java
+++ book_app/src/main/java/Book.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,35 @@
  * @author Jessica
  */
 public class Book {
+    String title;
+    ArrayList<Review> review;
+    ArrayList<Location> location;
+    Person author;
     
+    //constructor
+    Book (String title, Person author){
+        this.title = title;
+        this.author = author;
+        review = new ArrayList<>();
+        location = new ArrayList<>();
+    }
+    //methods
+    public void addReview(Book review){
+        review.addReview(review); 
+    }
+    public void deleteReview(Book review){
+        review.deleteReview(review);
+    }
+    public void addLocation(Book location){
+        location.addLocation(location);
+    }
+    public void deleteLocation(Book location){
+        location.deleteLocation(location);
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
 }
